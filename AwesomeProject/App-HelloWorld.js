928c6e0d2e7cdd49c10be83eefa764fa41bcb23e
Subramanian Murugan <sasubramanian_md@hotmail.com>
2023-07-06 11:54:22 +0530

    react native

---
+++ AwesomeProject/App-HelloWorld.js
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+    return <View>
+         <Text>Hello React Native</Text>
+         <Text>Hello React Native</Text>
+         <Text>Hello React Native</Text>
+         <Text>Hello React Native</Text>
+         <Text>Hello React Native</Text>
+         <Text>Hello React Native</Text>
+         <Text>Hello React Native</Text>
+         <Text>Hello React Native</Text>
+         <Text>Hello React Native</Text>
+         <Text>Hello React Native</Text>
+    </View>
+}
---
+++ AwesomeProject/App.js
@@ -0,0 +1,8 @@
+import { Greeter } from "./Greeter/Greeter";
+
+function App(){
+     return <>
+         <Greeter name={"Subramanian"} message={"Hello"} />
+     </>
+}
+export default App;
